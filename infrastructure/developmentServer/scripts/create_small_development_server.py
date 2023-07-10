d772813628107df494baf8206b56a96c5a4f5f40
Tim Loewel <tim@loewel.eu>
2023-07-10 16:45:18 +0200

    create user tim with ssh key

--- infrastructure/developmentServer/scripts/create_small_development_server.py
+++ infrastructure/developmentServer/scripts/create_small_development_server.py
@@ -9,22 +9,19 @@ from wait_until_finished import wait_until_finished
 load_dotenv()
 
 HETZNER_API_TOKEN = os.getenv('HETZNER_API_TOKEN')
-SSH_PUBLIC_KEY = os.getenv('SSH_PUBLIC_KEY')
-
+DEVELOPMENT_SERVER_SSH_PUBLIC_KEY = os.getenv('DEVELOPMENT_SERVER_SSH_PUBLIC_KEY')
+DEVELOPMENT_SERVER_SSH_KEY_NAME = "rootKey"
 VOLUME_NAME="TimDevelopmentDrive"
 SERVER_NAME="TimDevelopmentServer"
 
-print("the variable SSH_PUBLIC_KEY is none? " + str(SSH_PUBLIC_KEY is None))
-print("the variable SERVER_NAME is none? " + str(SERVER_NAME is None))
-
 client = Client(token=HETZNER_API_TOKEN)
-existing_keys = client.ssh_keys.get_all()
 
-if (len(existing_keys) == 0):
+if (client.ssh_keys.get_by_name(DEVELOPMENT_SERVER_SSH_KEY_NAME) is None):
     print("no ssh key found, creating new key")
-    client.ssh_keys.create("root key", SSH_PUBLIC_KEY)
+    client.ssh_keys.create(DEVELOPMENT_SERVER_SSH_KEY_NAME, DEVELOPMENT_SERVER_SSH_PUBLIC_KEY)
 
-keys = client.ssh_keys.get_all()
+
+keys_for_server = client.ssh_keys.get_all()
 existing_servers = client.servers.get_all()
 
 dc = client.datacenters.get_by_name("nbg1-dc3")
@@ -35,7 +32,7 @@ create_server_response = client.servers.create(
     name = SERVER_NAME,
     server_type=ServerType(name="cx11"),
     image=Image(name="ubuntu-20.04"),
-    ssh_keys=keys,
+    ssh_keys=keys_for_server,
     datacenter=dc,
 )
 create_server_response.action.wait_until_finished()
@@ -44,7 +41,7 @@ wait_until_finished(create_server_response.next_actions, "waiting for server nex
 
 
 new_server = client.servers.get_by_name(SERVER_NAME)
-
+new_server.model.
 volume = client.volumes.get_by_name(VOLUME_NAME)
 print("volume " + str(volume.model.name))
 # create_volume_response = client.volumes.create(name=volume_name, size=10, server=new_server, format="ext4", automount=True,
@@ -59,3 +56,8 @@ volume.attach(new_server, automount=True)
 
 print("new server can be found under")
 print(new_server.public_net.ipv4.dns_ptr)
+
+#  write the new server to the inventory file
+f = open('../ansible/inventory', 'w', encoding="utf-8")
+f.write(new_server.public_net.ipv4.dns_ptr + "\n")
+f.close()
